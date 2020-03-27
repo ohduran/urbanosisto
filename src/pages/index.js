191d06ab65e3e4130aae02a6b27e45b4832621ba
ohduran <alvaro.duranb@hotmail.com>
2020-03-27 15:50:05 +0100

    tailwind config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
-import React from "react"
+import React from "react";
+import '../styles/index.css';
 
-export default () => <div>Hello world!</div>
+export default () => <div><h1 class="text-center text-4xl text-blue-500 font-bold">Hello, world</h1></div>
