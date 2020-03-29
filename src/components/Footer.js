a53d37fcbc07e3d6881ba12d428bdc9cf1d16e46
ohduran <alvaro.duranb@hotmail.com>
2020-03-29 23:02:26 +0200

    sticky footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ import '../styles/index.css';
 const Footer = ({ className }) => {
 
     return (
-      <footer className={"bg-gray-300 pt-6 pb-16 grid sticky bottom-0 left-0 w-full" + className}>
+      <footer className={"bg-gray-800 pt-6 pb-16 grid sticky bottom-0 left-0 w-full" + className}>
         <div className="grid justify-center">
-          <h2 class="font-family-pacifico text-center">Subscríbete y consigue un <span className="text-teal-500">10% de descuento </span><br />en artículos de Nueva Colección</h2>
-          <input type="email" class="w-3/4 h-8 rounded-lg shadow-md focus:outline-none mx-auto mt-4 placeholder-gray-400 font-family-open-sans" placeholder="  Introduce tu email"/>
+          <h2 class="font-family-pacifico text-center text-gray-400">Subscríbete y consigue un <span className="text-teal-500">10% de descuento </span><br />en artículos de Nueva Colección</h2>
+          <input type="email" class="w-3/4 h-8 rounded-lg shadow-md focus:outline-none mx-auto mt-4 placeholder-gray-800 font-family-open-sans" placeholder="  Introduce tu email"/>
         </div>
       </footer>
     )
