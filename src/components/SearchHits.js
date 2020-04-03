51c5300a4357f0bf50f9128b13fe1c75b056f7b0
ohduran <alvaro.duranb@hotmail.com>
2020-04-03 16:24:07 +0200

    search by algolia

--- src/components/SearchHits.js
+++ src/components/SearchHits.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { connectHits } from 'react-instantsearch-dom';
+import { connectHits, PoweredBy } from 'react-instantsearch-dom';
+
 import '../styles/index.css';
 
 
 const Hits = ({ className, hits }) => (
-  <ol className={`bg-orange-200 absolute md:mt-4 py-2 rounded-lg shadow-xl font-family-montserrat ${className}`} style={{ display: 'none'}}>
+  <ol className={`bg-orange-200 absolute mt-8 py-2 rounded-lg shadow-xl font-family-montserrat ${className}`} style={{ display: 'none'}}>
     {hits.map(hit => (
-      <li className="px-4 py-2 rounded hover:bg-orange-500 hover:text-white text-xs md:text-sm" key={hit.objectID}>{hit.tagLine}</li>
+      <li className="px-4 py-2 rounded text-xs md:text-sm hover:bg-orange-500 hover:text-white" key={hit.objectID}>{hit.tagLine}</li>
     ))}
+    <li className="px-4 py-2 rounded text-xs"
+      key={'algolia'}>
+      <PoweredBy />
+    </li>
   </ol>
 );
 
