49de2d02469aa2be52693e530d15c8a35ea9f3e9
ohduran <alvaro.duranb@hotmail.com>
2020-04-04 12:56:26 +0200

    fix search hits position

--- src/components/SearchHits.js
+++ src/components/SearchHits.js
@@ -6,7 +6,7 @@ import '../styles/index.css';
 
 
 const Hits = ({ className, hits }) => (
-  <ol className={`bg-orange-200 absolute z-10 mt-8 py-2 rounded-lg shadow-xl font-family-montserrat ${className}`} style={{ display: 'none'}}>
+  <ol className={`bg-orange-200 absolute z-10 mt-8 mr-12 py-2 rounded-lg shadow-xl font-family-montserrat ${className}`} style={{ display: 'none'}}>
     {hits.map(hit => (
       <li className="px-4 py-2 rounded text-xs md:text-sm hover:bg-orange-500 hover:text-white" key={hit.objectID}>
         <Link to={`items/${hit.slug}`}>{hit.tagLine}</Link>
